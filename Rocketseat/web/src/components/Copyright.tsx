1eefb5c58b5ce5df6e6d42b06a78ec437506782a
Thiagoo.c <127368836+thiagoooc@users.noreply.github.com>
2023-06-21 20:05:39 -0300

    Add files via upload

---
+++ Rocketseat/web/src/components/Copyright.tsx
@@ -0,0 +1,14 @@
+export function Copyright() {
+  return (
+    <div className="fpnt-alt inline-block rounded-full bg-green-500 px-2 py-2 text-sm uppercase leading-none text-black hover:bg-green-600">
+      Feito com 💜 pelo{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/thiagocesarmagalhães/"
+      >
+        Thiago
+      </a>
+    </div>
+  )
+}
